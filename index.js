f3a45e47a1f2fd502606a198a75be8f1ceced32b
mitulchy07 <mitul9616@gmail.com>
2022-10-24 19:32:57 +0200

    categories added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const Port = process.env.port || 3000;
+const courseCollection = require('./Data/products.json');
 
 app.get('/', (req, res) => {
   res.send('API is running.');
+});
+
+app.get('/categories', (req, res) => {
+  res.send(courseCollection);
 });
 
 app.listen(Port, () => {
