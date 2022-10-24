7472a18597289d6cebce0db481ef05b4802306a2
mitulchy07 <mitul9616@gmail.com>
2022-10-24 20:12:05 +0200

    items separeted

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const Port = process.env.port || 3000;
-const courseCollection = require('./Data/products.json');
+const cors = require('cors');
+app.use(cors());
+
+const categories = require('./Data/categories.json');
+const products = require('./Data/products.json');
 
 app.get('/', (req, res) => {
-  res.send('API is running.');
+  res.send('Products API Running');
 });
 
 app.get('/categories', (req, res) => {
-  res.send(courseCollection);
+  res.send(categories);
+});
+
+app.get('/products', (req, res) => {
+  res.send(products);
+});
+
+app.get('/categories/:id', (req, res) => {
+  const id = req.params.id;
+  const category_products = products.filter((n) => n.category === id);
+  res.send(category_products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedproducts = products.find((n) => n.id === id);
+  res.send(selectedproducts);
 });
 
 app.listen(Port, () => {
-  console.log('Server is running on port:', Port);
+  console.log('Products Server running on port', Port);
 });
